41c4f3ec08f0dd5e4cc3abc6d4e89c17b10e6cd9
Tamerlan Zhabassov <zhbssv@mail.ru>
2019-02-09 01:48:04 +0600

    Рефакторинг основных полей ввода

--- src/test/java/EditMainInformation.java
+++ src/test/java/EditMainInformation.java
@@ -1,13 +1,10 @@
 
-import java.util.regex.Pattern;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.*;
 import static org.testng.Assert.*;
 import org.openqa.selenium.*;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.Select;
 
 public class EditMainInformation {
   private WebDriver driver;
@@ -21,31 +18,63 @@ public class EditMainInformation {
     driver = new ChromeDriver();
     baseUrl = "https://www.training.by/";
     driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    login("trainigby@bk.ru", "Astana2019");
+  }
+
+  private void login(String email, String password) {
     driver.get(baseUrl);
-    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='О тренингах'])[1]/following::button[1]")).click();
+    driver.findElement(By.className("uui-button")).click();
     driver.findElement(By.id("Name")).click();
     driver.findElement(By.id("Name")).clear();
-    driver.findElement(By.id("Name")).sendKeys("trainigby@bk.ru");
+    driver.findElement(By.id("Name")).sendKeys(email);
     driver.findElement(By.id("Password")).click();
     driver.findElement(By.id("Password")).clear();
-    driver.findElement(By.id("Password")).sendKeys("Astana2019");
+    driver.findElement(By.id("Password")).sendKeys(password);
     driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Войти'])[2]/following::button[2]")).click();
   }
 
   @Test
   public void testEditMainInformation() throws Exception {
+    goToProfile();
+    editProfile();
+    editLastName("Kushukova");
+    editFirstName("Madina");
+    editNativeName("Economist");
+    editMobileNumber("7029736270");
+    submitionEditionProfile();
+  }
+
+  private void submitionEditionProfile() {
+    driver.findElement(By.xpath("//div[@class='userProfile-buttons']/button")).click();
+  }
+
+  private void editMobileNumber(String number) {
+    driver.findElement(By.name("mobilePhone")).clear();
+    driver.findElement(By.name("mobilePhone")).sendKeys(number);
+  }
+
+  private void editNativeName(String nativeName) {
+    driver.findElement(By.name("nativeName")).clear();
+    driver.findElement(By.name("nativeName")).sendKeys(nativeName);
+  }
+
+  private void editFirstName(String firstName) {
+    driver.findElement(By.name("firstName")).clear();
+    driver.findElement(By.name("firstName")).sendKeys(firstName);
+  }
+
+  private void editLastName(String lastName) {
+    driver.findElement(By.name("lastName")).clear();
+    driver.findElement(By.name("lastName")).sendKeys(lastName);
+  }
+
+  private void editProfile() {
+    driver.findElement(By.xpath("//a[contains(text(),'Редактировать')]")).click();
+  }
+
+  private void goToProfile() {
     driver.findElement(By.className("profile-name")).click();
     driver.findElement(By.linkText("Профиль")).click();
-    driver.findElement(By.xpath("//a[contains(text(),'Редактировать')]")).click();
-    driver.findElement(By.name("lastName")).clear();
-    driver.findElement(By.name("lastName")).sendKeys("Zhabassov");
-    driver.findElement(By.name("firstName")).clear();
-    driver.findElement(By.name("firstName")).sendKeys("Kasimbek");
-    driver.findElement(By.name("nativeName")).clear();
-    driver.findElement(By.name("nativeName")).sendKeys("Energetik");
-    driver.findElement(By.name("mobilePhone")).clear();
-    driver.findElement(By.name("mobilePhone")).sendKeys("9969371406");
-    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Отмена'])[1]/following::button[1]")).click();
   }
 
   @AfterClass(alwaysRun = true)
